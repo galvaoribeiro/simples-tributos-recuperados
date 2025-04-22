5b150ec55e34a9427111323e96ad389260bf9db6
galvaoribeiro <ronaldo_fab@hotmail.com>
2025-04-21 21:53:09 -0400

    removendo olhos mais treinados

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const benefits = [
   {
     title: 'Economia comprovada de até 30% nos tributos',
-    description: 'Nossa tecnologia identifica oportunidades de economia que passam despercebidas até pelos olhos mais treinados.',
+    description: 'Nossa tecnologia identifica oportunidades de economia que passam despercebidas',
     icon: (
       <svg className="w-10 h-10 text-auditor-skyBlue" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
